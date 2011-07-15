56c8a23f810e85d45a21fd75a4d30ee54e03fb6a
Alex McNamara <alex.mcnamara@gmail.com>
2011-07-14 23:49:50 -0400

    fixed shallow-copy head/tail pointer corruption, fixed edge-case bugs in insert and reverse

--- Python/LList.py
+++ Python/LList.py
@@ -1,3 +1,4 @@
+## Error objects
 class CircularDependency(Exception):
     def __init__(self, msg = ""):
         self.msg = msg
@@ -10,7 +11,9 @@ class NodeNotFound(Exception):
     def __str__(self):
         return repr(self.msg)
 
+## Composite objects of the LList structure
 class Node(object):
+    '''Node container for LList implementation'''
     datum = None
     next  = None
     def __init__(self, datum):
@@ -19,29 +22,47 @@ class Node(object):
     def __str__(self):
         return repr(self.datum)
 
+class Endpoint(object):
+    '''This is to make LList copy-safe, since its operations are in place and head/tail pointers can be corrupted on shallow copy.'''
+    node = None
+    def __init__(self, point = None):
+        self.node = point
+    def __eq__(self, comp):
+        return comp == self.node
+
+## Linked List implementation
 class LList(object):
-    head = None
-    tail = None
+    '''Linked List implementation, operations are in-place, structure is shallow-copy safe.'''
     def __init__(self):
-        pass
+        self.head = Endpoint()
+        self.tail = Endpoint()
     def __str__(self):
         return " -> ".join(map(lambda node: str(node), self))
+    def __getitem__(self, idx):
+        if type(idx) == type(slice):
+            pass
+        else:
+            pass
     def __add__(self, datum):
         node = Node(datum)
-        if self.head == None:
-            self.head = node
-        if self.tail != None:
-            self.tail.next = node
-        self.tail = node
+        if self.head.node == None:
+            self.head.node = node
+        if self.tail.node != None:
+            self.tail.node.next = node
+        self.tail.node = node
         return self
     def __sub__(self, node):
+        if self.head.node == node:
+            self.head.node = self.head.node.next
+            if self.tail.node == node:
+                self.tail.node = None
         prev = filter(lambda test: test.next == node, self)
         if prev: 
             prev[0].next = node.next
         return self
     def __iter__(self):
         self.iternode = None
-        self.iterfast = self.head
+        self.iterfast = self.head.node
         return self
     def next(self):
         if self.head == None:
@@ -51,7 +72,7 @@ class LList(object):
         if self.iterfast and self.iterfast == self.iternode:
             raise CircularDependency
         if self.iternode == None:
-            self.iternode = self.head
+            self.iternode = self.head.node
             return self.iternode
         if self.iternode.next == None:
             raise StopIteration
@@ -61,9 +82,9 @@ class LList(object):
     def insert(self, datum, next = None):
         node = Node(datum)
         prev = None
-        if not next or next == self.head:
-            next = self.head
-            self.head = node
+        if not next or not self.head or next == self.head.node:
+            next = self.head.node
+            self.head.node = node
         else:
             try:
                 prev = filter(lambda test: test.next == next, self)[0]
@@ -73,11 +94,11 @@ class LList(object):
         node.next = next
         return self
     def reverse(self):
-        if not self.head.next:
+        if not self.head or not self.head.node or not self.head.node.next:
             return self
         prev  = None
         pprev = None
-        if self.head.next and self.head.next.next:
+        if self.head.node.next and self.head.node.next.next:
             for node in self:
                 if not prev:
                     prev = node
@@ -88,12 +109,12 @@ class LList(object):
                     continue
                 prev.next = pprev
                 pprev, prev = prev, node
-        elif self.head.next:
-            prev  = self.tail
-            pprev = self.head
+        else:
+            prev  = self.tail.node
+            pprev = self.head.node
         prev.next = pprev
-        self.head, self.tail = self.tail, self.head
-        self.tail.next = None
+        self.head.node, self.tail.node = self.tail.node, self.head.node
+        self.tail.node.next = None
         return self
     def getNodes(self, datum):
         return filter(lambda node: node.datum == datum, self)
