bf3d77379e698ec3bd9d9b6624f8480d5933403a
Alex McNamara <alex.mcnamara@gmail.com>
2011-08-18 00:32:44 -0400

    Small refactoring to improve golf score (char count)

--- 4Clojure/91.clj
+++ 4Clojure/91.clj
@@ -5,20 +5,19 @@
 ;;  - You will be given a set of tuples representing the edges of a graph. Each member of a tuple being a vertex/node in the graph.
 ;;  - Each edge is undirected (can be traversed either direction). 
 
-;; Golf score: 296 (minified)
+;; Golf score: 224 (minified)
 
 (fn [full-graph]
   (letfn [(connected [node graph]                                                                                                                                    
-            (filter (complement nil?)                                                                                                                               
-                    (map (fn [i] (if (= node (first i)) (second i)                                                                                                     
-				     (if (= node (second i)) (first i))))                                                                                              
-                         (seq graph))))                                                                                                                                 
-         (nodes [graph]                                                                                                                                                         
-            (distinct (flatten (seq graph))))]                                                                                                                               
-    (loop [result [(-> full-graph first first)]]                                                                                                  
-      (if (empty? (clojure.set/difference (set (nodes full-graph)) (set result)))                                                                                         
-	true                                                                                                                                                  
-	(if (empty? (clojure.set/difference (set (nodes (map (fn [i] (connected i full-graph)) result)))                                                                
-					    (set result)))                                                                                                         
-	  false               
-	  (recur (into result (nodes (map (fn [i] (connected i full-graph)) result)))))))))
+		     (filter (complement nil?)                                                                                                                               
+			     (map (fn [i] (if (= node (first i)) (second i)                                                                                                     
+					      (if (= node (second i)) (first i))))                                                                                              
+				  (seq graph))))                                                                                                                                 
+	  (nodes [graph]                                                                                                                                                         
+		 (flatten (seq graph)))]                                                                                                                               
+    (loop [result [(first (node full-graph))]]                                                        
+      (if (= (set (nodes full-graph)) (set result)))                                                                                         
+	true
+	(if (= (set (nodes (map (fn [i] (connected i full-graph)) result))) (set result))
+	  false
+	  (recur (into result (nodes (map (fn [i] (connected i full-graph)) result))))))))
