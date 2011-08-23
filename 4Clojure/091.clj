bade0435fa26f06bec8956b773c7d89330e27352
Alex McNamara <alex.mcnamara@gmail.com>
2011-08-23 12:24:37 -0400

    Refactored.

--- 4Clojure/091.clj
+++ 4Clojure/091.clj
@@ -5,17 +5,16 @@
 ;;  - You will be given a set of tuples representing the edges of a graph. Each member of a tuple being a vertex/node in the graph.
 ;;  - Each edge is undirected (can be traversed either direction). 
 
-;; Golf score: 224 (minified)
+;; Golf score: 208 (minified)
 
 (fn [full-graph]
-  (letfn [(connected [node graph]                                                                                                                                   
-		     (filter (complement nil?)                                                                                                                      
-			     (map #(if (= node (first  %)) (second %)
-                                   (if (= node (second %)) (first  %))))
-                                  (seq graph))))
-	  (nodes [graph]
-            (flatten (seq graph)))]                                                                                                                               
-    (loop [result [(first (node full-graph))]]                                                        
+  (let [connected #(filter (complement nil?)                                                                                                                      
+                           (map (fn [[i j]]
+                                  (if (= % i) j
+                                  (if (= % j) i)))
+                           (seq graph)))
+        nodes     #(flatten (seq %))]                                                                                                                            
+    (loop [result [(first (nodes full-graph))]]                                                        
       (if (= (set (nodes full-graph)) (set result))
         true
         (if (= (set (nodes (map (fn [i] (connected i full-graph)) result))) (set result))
