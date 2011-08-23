39d309ef2b0b4202cc59219e01a555a80d19dd22
Alex McNamara <alex.mcnamara@gmail.com>
2011-08-23 11:31:30 -0400

    renaming for consistency

---
+++ 4Clojure/091.clj
@@ -0,0 +1,23 @@
+;; See http://www.4clojure.com/problem/91
+
+;; Given a graph, determine whether the graph is connected. A connected graph is such that a path exists between any two given nodes.
+;;  - Your function must return true if the graph is connected and false otherwise.
+;;  - You will be given a set of tuples representing the edges of a graph. Each member of a tuple being a vertex/node in the graph.
+;;  - Each edge is undirected (can be traversed either direction). 
+
+;; Golf score: 224 (minified)
+
+(fn [full-graph]
+  (letfn [(connected [node graph]                                                                                                                                   
+		     (filter (complement nil?)                                                                                                                      
+			     (map #(if (= node (first  %)) (second %)
+                                   (if (= node (second %)) (first  %))))
+                                  (seq graph))))
+	  (nodes [graph]
+            (flatten (seq graph)))]                                                                                                                               
+    (loop [result [(first (node full-graph))]]                                                        
+      (if (= (set (nodes full-graph)) (set result))
+        true
+        (if (= (set (nodes (map (fn [i] (connected i full-graph)) result))) (set result))
+          false
+          (recur (into result (nodes (map (fn [i] (connected i full-graph)) result)))))))))
